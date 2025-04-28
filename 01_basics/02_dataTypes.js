10517cf1106386e99899c476ffca360ab1b0858a
Muhammad Atif Yar <147138122+atifyar7@users.noreply.github.com>
2025-04-28 11:15:13 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,13 @@
+"use strict" //treat all js code as newer version
+
+// allert (3+3) //we are using node js, not browser
+
+let age= 23
+let name= "Atif Yar"
+let isLoggedIn= true
+
+//Data Types: number, string
+console.log(typeof isLoggedIn)
+
+//datatype of null = object
+//datatype of undefined = undefined
