5276237a12041c694dc39ba0fdb19bfb726bac05
Muhammad Atif Yar <147138122+atifyar7@users.noreply.github.com>
2025-04-28 18:20:02 +0000

    Stack & Heap Memory

---
+++ 01_basics/04_stack_heap_memory.js
@@ -0,0 +1,21 @@
+// ======== Stack =======
+
+let youTube = "alfaz_i_ishq"
+let newYotTube = youTube
+
+newYotTube = "Alfaz i ishq"
+
+console.log(youTube);
+console.log(newYotTube);
+
+// ======== Heap ========
+let student1 = {
+    name:"Atif Yar",
+    age: 21,
+}
+
+let student2 = student1
+
+student2.age = 23
+console.log(student1);
+console.log(student2);
